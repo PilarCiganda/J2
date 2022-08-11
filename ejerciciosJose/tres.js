5ca85b475278f41dea5e24cc16da9067916ce175
PilarCiganda <cigandapilar@gmail.com>
2022-08-11 15:36:48 -0300

    extras Jose

---
+++ ejerciciosJose/tres.js
@@ -0,0 +1,12 @@
+// Hacer una función que reciba la cantidad de horas trabajadas por una persona, y el valor de cada hora, y devuelva el salario correspondiente.
+
+const sueldo = (horas, valorHora) => {
+    const salario = horas * valorHora;
+    console.log(salario);
+}
+
+sueldo(160, 2000);
+
+
+
+
